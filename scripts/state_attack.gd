fe348368a8ed21192453cf12830a28e1f23630bd
Delvin <richardbegok03@gmail.com>
2024-11-30 16:27:55 +0700

    sword
    kasi sword ke player

---
+++ scripts/state_attack.gd
@@ -0,0 +1,32 @@
+class_name State_attack extends State
+@onready var walk :State=$"../walk"
+@onready var animation_player : AnimationPlayer = $"../../AnimationPlayer"
+@onready var  idle : State=$"../idle"
+var attacking:bool =false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func Enter() -> void :
+	player.UpdateAnimation("attack")
+	animation_player.animation_finished.connect(EndAttack)
+	attacking = true
+	pass
+func Exit() -> void :
+	
+	pass
+func process(_delta: float) -> State:
+	
+	player.velocity = Vector2.ZERO
+	if attacking == false :
+		if player.direction ==Vector2.ZERO:
+			return idle
+		else:
+			return walk
+	return null
+
+func Physics(_delta: float) -> State:
+	return null
+func HandleInput(_event: InputEvent) -> State:
+	return null
+func EndAttack(_newAnimName : String) -> void:
+	attacking = false
--- scripts/state_walk.gd
+++ scripts/state_walk.gd
@@ -2,7 +2,7 @@ class_name State_walk extends State
 
 @export var move_speed : float = 200.0
 @onready var idle : State = $"../idle"
-
+@onready var attack : State =$"../attack"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,4 +23,6 @@ func process(_delta: float) -> State:
 func Physics(_delta: float) -> State:
 	return null
 func HandleInput(_event: InputEvent) -> State:
+	if _event.is_action_pressed("attack"):
+		return attack
 	return null

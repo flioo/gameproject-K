abba407d1bb70d1393df1e5cbe52b1d47850a51e
Delvin <richardbegok03@gmail.com>
2024-12-03 11:16:32 +0700

    audio keren
    audio + shadow + sowrd effect

--- scripts/state_attack.gd
+++ scripts/state_attack.gd
@@ -1,14 +1,22 @@
 class_name State_attack extends State
+@export var Attack_sound:AudioStream
+@export_range(1.20,0.5) var decelarate_speed :float = 5.0
 @onready var walk :State=$"../walk"
 @onready var animation_player : AnimationPlayer = $"../../AnimationPlayer"
 @onready var  idle : State=$"../idle"
+@onready var sword_effect : AnimationPlayer =$"../../Sprite2D/attackeffect/AnimationPlayer"
+@onready var audio : AudioStreamPlayer2D =$"../../audio/AudioStreamPlayer2D"
 var attacking:bool =false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func Enter() -> void :
 	player.UpdateAnimation("attack")
+	sword_effect.play("Attack_"+player.animDirection())
 	animation_player.animation_finished.connect(EndAttack)
+	audio.stream = Attack_sound
+	audio.pitch_scale = randf_range(1.0 , 1.3)
+	audio.play()
 	attacking = true
 	pass
 func Exit() -> void :
@@ -17,7 +25,7 @@ func Exit() -> void :
 	pass
 func process(_delta: float) -> State:
 	
-	player.velocity = Vector2.ZERO
+	player.velocity-= player.velocity * decelarate_speed * _delta
 	if attacking == false :
 		if player.direction ==Vector2.ZERO:
 			return idle

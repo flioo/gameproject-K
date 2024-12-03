7aa5cb6dff67d974137a65c4af1ee8b5819dbd4f
flioo <kapuyugaming@gmail.com>
2024-12-03 20:58:12 +0700

    fix main menu

--- scripts/main-menu.gd
+++ scripts/main-menu.gd
@@ -3,10 +3,8 @@ extends Control
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://main scene/Main.tscn")
 
-
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene'/mainmenu/option-menu.tscn")
 
-
-func _on_about_us_pressed() -> void:
+func _on_exit_pressed() -> void:
 	get_tree().quit()

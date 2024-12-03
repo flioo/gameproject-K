3256dd069bffa3a989fd12a91f78c156e96ca03f
flioo <kapuyugaming@gmail.com>
2024-12-04 00:42:28 +0700

    rework again and fix

--- scene'/mainmenu/ui_ux_ingame.gd
+++ scene'/mainmenu/ui_ux_ingame.gd
@@ -1,4 +1,4 @@
 extends Control
 
 func _on_backingame_pressed() -> void:
-	get_tree().change_scene_to_file("res://assets/char/main-menu.tscn")
+	get_tree().change_scene_to_file("res://main scene/main_menu.tscn")

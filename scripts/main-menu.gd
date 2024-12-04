5d7e72d9e448da25fd8f240828cca6c5d1a07724
flioo <kapuyugaming@gmail.com>
2024-12-04 21:31:30 +0700

    main-menu theme
    add font minecraft-reguler

--- scripts/main-menu.gd
+++ scripts/main-menu.gd
@@ -1,10 +1,12 @@
 extends Control
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://main scene/Main.tscn")
+	get_tree().change_scene_to_file("res://main scene/main.tscn")
 
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene'/mainmenu/option-menu.tscn")
 
-func _on_exit_pressed() -> void:
+
+
+func _on_quit_pressed() -> void:
 	get_tree().quit()

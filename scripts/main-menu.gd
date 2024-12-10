7d5d9190c295fa6b9f388986a850d55484aad0d0
flioo <kapuyugaming@gmail.com>
2024-12-10 14:33:41 +0700

    main menu reconnect

--- scripts/main-menu.gd
+++ scripts/main-menu.gd
@@ -1,12 +1,10 @@
 extends Control
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://main scene/main.tscn")
+	get_tree().change_scene_to_file("res://main scene/Main.tscn")
 
 func _on_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://scene'/mainmenu/option-menu.tscn")
-
-
+	get_tree().change_scene_to_file("res://GeneralNodes/uiux/option-menu.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()

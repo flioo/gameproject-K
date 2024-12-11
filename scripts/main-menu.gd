702150f601a4d0cd01b5e4303bcd359ad1dda21e
flioo <105304708+flioo@users.noreply.github.com>
2024-12-11 08:43:59 +0700

    mainmenu connect

--- scripts/main-menu.gd
+++ scripts/main-menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://main scene/Main.tscn")
+	get_tree().change_scene_to_file("res://Main Scene/Main.tscn")
 
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://GeneralNodes/uiux/option-menu.tscn")
